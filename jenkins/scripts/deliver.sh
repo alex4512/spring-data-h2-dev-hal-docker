45b1eda2ef9ee8d44d038dabf78940fbd80977be
alex4512 <35726276+alex4512@users.noreply.github.com>
2020-04-20 15:11:49 +0100

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,4 +23,8 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar --server.port=8091
+docker build -t springio/gs-spring-boot-docker .
+set +x
+set -x
+docker run -p 8091:8091 -t springio/gs-spring-boot-docker
+set +x
